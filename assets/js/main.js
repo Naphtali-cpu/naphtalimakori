5d449ca537166032233a78ce2f9c1e90498f52c9
Naphtali-cpu <naphtali919@gmail.com>
2021-10-30 21:55:28 +0300

    Added first files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCCzg6_efgCijuCZyj59SZSDB13JMARcnQ",
+  authDomain: "naphtali-makori.firebaseapp.com",
+  projectId: "naphtali-makori",
+  storageBucket: "naphtali-makori.appspot.com",
+  messagingSenderId: "226042992202",
+  appId: "1:226042992202:web:6709598fd09ef5cb40b8b3",
+  measurementId: "G-B5QHH591Q8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 /*==================== SHOW MENU ====================*/
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
